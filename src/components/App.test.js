614a7039d0d12e4ca7e8b7e0bc640960607f87bc
Bryan Yee <yee.bryan.c@gmail.com>
2022-03-07 16:34:15 -0800

    Add test for stock details page with mocked api data after user navigation
    https://testing-library.com/docs/ecosystem-user-event

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import App from './App';
@@ -13,4 +14,12 @@ it('renders the index page with /stocks api data', async () => {
   render(<App />);
   await screen.findByTestId('stock-index-content');
   expect(screen.getByText('Test Inc.')).toBeVisible();
+});
+
+it('renders the stock details page with stock api data', async () => {
+  render(<App />);
+  await screen.findByTestId('stock-index-content');
+  userEvent.click(screen.getByText('TEST-TICKER'));
+  await screen.findByTestId('stock-details-content');
+  expect(screen.getByText('Test Inc. (TEST-TICKER)')).toBeVisible();
 });
--- src/mockHandlers.js
+++ src/mockHandlers.js
@@ -2,26 +2,31 @@ import { rest } from 'msw';
 
 import { URLS } from './constants';
 
+const stockStub = {
+  "id": 0,
+  "ticker": "TEST-TICKER",
+  "name": "Test Inc.",
+  "currentPrice": 174.78,
+  "historicalPrices": [
+    {
+      "date": "2022-01-14",
+      "open": 171.339996,
+      "high": 173.779999,
+      "low": 171.089996,
+      "close": 173.070007,
+      "volume": 80355000
+    }
+  ]
+};
+
 const handlers = [
   rest.get(URLS.stocks, (req, res, ctx) => {
-    const json = [{
-      "id": 0,
-      "ticker": "TEST",
-      "name": "Test Inc.",
-      "currentPrice": 174.78,
-      "historicalPrices": [
-        {
-          "date": "2022-01-14",
-          "open": 171.339996,
-          "high": 173.779999,
-          "low": 171.089996,
-          "close": 173.070007,
-          "volume": 80355000
-        }
-      ]
-    }];
-    return res(ctx.json(json));
-  })
+    return res(ctx.json([stockStub]));
+  }),
+
+  rest.get(`${URLS.stocks}?ticker=TEST-TICKER`, (req, res, ctx) => {
+    return res(ctx.json(stockStub));
+  }),
 ];
 
 export default handlers;
